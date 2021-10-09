aed19af8c118b57f8ed5b4d662608c12ab947e66
datapaf <georgyandryuschenko@gmail.com>
2021-10-09 21:58:19 +0300

    feedback saving finalized 1

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,15 +9,40 @@ import requests
 import os
 load_dotenv()
 
+SERVER_ADDR = os.getenv("SERVER_ADDR")
 
 def handle_callback(update: Update, context: CallbackContext) -> None:
 
-    if update.callback_query.data == "like":
-        print('liked')
-    elif update.callback_query.data == "dislike":
-        print('disliked')
-    
-    context.bot.answer_callback_query(update.callback_query.id)
+    callback_data_list = json.loads(update.callback_query.data)
+    feedback = {
+        "image_url": callback_data_list[1],
+        "is_good": callback_data_list[0]
+    }
+
+    try:
+        response = requests.post(
+            f"http://{SERVER_ADDR}/api/v1/feedback",
+            data=feedback
+        )
+    except:
+        context.bot.answer_callback_query(
+            callback_query_id=update.callback_query.id,
+            text='Oops... Somehow we cannot save your feedback 😔'
+        )
+
+
+    if response.ok:
+        if response.text == 'already saved':
+            callback_answer_text = "We've already got your feedback 😏"
+        else:
+            callback_answer_text = 'Thank you for feedback!'
+    else:
+        callback_answer_text = 'Oops... Somehow we cannot save your feedback 😔'
+
+    context.bot.answer_callback_query(
+        callback_query_id=update.callback_query.id,
+        text=callback_answer_text
+    )
 
 
 def handle_text(update: Update, context: CallbackContext) -> None:
@@ -43,10 +68,20 @@ def image_processing(update: Update, context: CallbackContext) -> None:
     file_bytes = bytearray(f.read())
     
     # Send image to the server
-    SERVER_ADDR = os.getenv("SERVER_ADDR")
-    response = requests.post(SERVER_ADDR, files={'image': (file_name, file_bytes)})
-    
+    try:
+        response = requests.post(
+            f"http://{SERVER_ADDR}/api/v1/pictures",
+            files={'image': (file_name, file_bytes)}
+        )
+    except:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Couldn't process your image 😔\nPlease send the image again"
+        )
+        raise Exception('failed to send the image')
+
     # Receive caption from server
+    image_url = json.loads(response.text)['imageURL']
     caption = json.loads(response.text)['caption']
 
     # reply the user with the caption and the buttons to rate the caption
@@ -57,8 +92,14 @@ def image_processing(update: Update, context: CallbackContext) -> None:
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("👍", callback_data='like'),
-                    InlineKeyboardButton("👎", callback_data='dislike')
+                    InlineKeyboardButton(
+                        "👍",
+                        callback_data=json.dumps(["1", image_url])
+                    ),
+                    InlineKeyboardButton(
+                        "👎",
+                        callback_data=json.dumps(["0", image_url])
+                    )
                 ]
             ]
         )
